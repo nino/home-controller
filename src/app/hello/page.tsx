438947d92db6b7502d8c3120235560af38158ba1
Nino Annighoefer <nino@ninoan.com>
2024-10-30 18:13:10 +0000

    Add first page

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * First real page. This one is just here for me to figure out the new way
+ * NextJS does routing.
+ */
+export default function Home() {
+  return (
+    <div>
+      <h1 className="m-8 text-center text-4xl">Hello</h1>
+    </div>
+  );
+}
